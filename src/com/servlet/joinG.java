f707fa9bbcbf2b276116f5b06fd6659eb01c117a
jayce <2434128704@qq.com>
2019-06-24 09:19:27 +0800

    modifier

---
+++ src/com/servlet/joinG.java
@@ -0,0 +1,57 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.Config.config;
+import com.model.Tank;
+import com.model.User;
+import com.util.JdbcUtil;
+
+/**
+ * Servlet implementation class joinG
+ */
+@WebServlet("/joinG")
+public class joinG extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public joinG() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+				String name = (String) request.getAttribute("username");
+				ArrayList<Tank> tl = config.tl;
+				Tank t = new Tank();
+				t.setName(name);
+				tl.add(t);
+				request.setAttribute("name", name);
+				request.getRequestDispatcher("main.jsp").forward(request, response);
+			
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
